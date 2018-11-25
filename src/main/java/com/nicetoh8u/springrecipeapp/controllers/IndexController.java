90bcd8f1de14e22584425fb617032906a5438ea8
nice2h8u <nicetoh8u@gmail.com>
2018-11-25 19:45:00 +0300

    lombok and refactoring were added. closes #9

---
+++ src/main/java/com/nicetoh8u/springrecipeapp/controllers/IndexController.java
@@ -0,0 +1,27 @@
+package com.nicetoh8u.springrecipeapp.controllers;
+
+import com.nicetoh8u.springrecipeapp.Service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @Autowired
+    RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("")
+    public String index(Model model){
+       model.addAttribute("recipes",recipeService.getRecipes());
+        log.debug("getting index page");
+        return "index";
+    }
+}
