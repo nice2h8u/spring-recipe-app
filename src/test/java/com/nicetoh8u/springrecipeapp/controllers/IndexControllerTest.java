cb6432797972128591dbf556fc28f25b6d93fb83
nice2h8u <nicetoh8u@gmail.com>
2019-01-23 16:50:35 +0300

    added some tests

--- src/test/java/com/nicetoh8u/springrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/nicetoh8u/springrecipeapp/controllers/IndexControllerTest.java
@@ -4,15 +4,23 @@ import com.nicetoh8u.springrecipeapp.Service.RecipeService;
 import com.nicetoh8u.springrecipeapp.model.Recipe;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -30,15 +38,42 @@ public class IndexControllerTest {
         indexController = new IndexController(recipeService);
     }
 
+
+    @Test
+    public void testMockMvc() throws Exception {
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+    }
+
     @Test
     public void index() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
 
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
 
+        Mockito.when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        //when
         String viewName = indexController.index(model);
+
+
+        //then
         assertEquals("index", viewName);
         Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
         Mockito.verify(model, Mockito.times(1))
-                .addAttribute(Mockito.eq("recipes"), Mockito.anySet());
+                .addAttribute(Mockito.eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setIndexController = argumentCaptor.getValue();
+        assertEquals(2, setIndexController.size());
 
     }
 }
